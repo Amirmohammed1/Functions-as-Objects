e19ccc2489533da4ee902de91f95f1909df7e8e5
Amir Mohammed <amir.mohammed00@gmail.com>
2023-04-02 05:36:08 +0000

    done

---
+++ findWaldoAnon.js
@@ -0,0 +1,13 @@
+const findWaldo = function(names, found) {
+  names.forEach(function(name, index) {
+    if (name === "Waldo") {
+      found(index);
+    }
+  });
+}
+
+const actionWhenFound = function(index) {
+  console.log(`Found Waldo at index ${index}!`);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
